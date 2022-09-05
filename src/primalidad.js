ed1439d72fbf6d2f5fc23d594505a83d3766e854
G-Delgado <gabriel.delgado1@u.icesi.edu.co>
2022-09-05 11:56:38 -0500

    primalidad

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,17 @@
 const trialDivision = (number) => {
-  // your code here
+  let prime = false
+  if (Number.isInteger(number) && number >= 0) {
+    let count = 0;
+    for (let i = number; i >= 1; i--) {
+      if ( (number % i) == 0) {
+        count++
+      }
+    }
+    if (count == 2) {
+      prime = true
+    }
+  }
+  return prime
 }
 
 module.exports = trialDivision;
