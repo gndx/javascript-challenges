4f2945d90b554fad8b2e9481cdd05acad82a0354
DESKTOP-T2DVTIK\LMARTINEZ <lmartinez@optimeconsulting.com>
2022-09-05 10:47:47 -0500

    challenge resuelto

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,11 @@
 const fibonacci = (n) => {
   // your code here
+  if (n <= 1) return [n]
+  let array = [1, 1]
+  for (let i = 2; i < n; i++) {
+    array.push(array[i -1] + array[i - 2]);
+  }
+  return array
 }
 
 module.exports = fibonacci;
