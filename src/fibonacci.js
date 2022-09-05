e140b8985d41acf11b5c238459a555aa4eb9dff4
Flavius Stan <flaviusns@outlook.com>
2022-09-05 18:17:09 +0200

    Pruebas realizadas con exito

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,14 @@
 const fibonacci = (n) => {
-  // your code here
+  let x = 0, y = 1, num;
+  let arr = [];
+  num = x + y;
+  for (let i = 1; i <= n; i++) {
+    arr.push(num);
+    num = x + y;
+    x = y;
+    y = num;
+  }
+  return arr;
 }
 
 module.exports = fibonacci;
