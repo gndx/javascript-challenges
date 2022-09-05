ab6b7c4ee5a63047e8b552b82000521db1a1609d
Christian Alexánder <chrisalex.w@gmail.com>
2022-09-05 13:25:09 -0500

    test_resolution

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,9 @@
 const factorial = (number) => {
-  // your code here
+  let result = 1
+  for (var i = 1; i <= number; i++) {
+    result = result * i
+  }
+  return result
 }
 
 module.exports = factorial;
