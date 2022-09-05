924ebe8aeeb45d6c5244809d90cbb9a2b652f336
Diego Andrés Torres Riveros <88874892+diegotorres3006@users.noreply.github.com>
2022-09-05 15:58:18 -0500

    Ejercicio 3

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,14 @@
 const fibonacci = (n) => {
-  // your code here
+  var fibonacci = [];
+if (n===1){
+    fibonacci=[1];
+}else{
+fibonacci[0] = 1;
+fibonacci[1] = 1;
+  for (var i = 2; i < n; i++) {
+  fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+  }
 }
-
+return(fibonacci)
+}
 module.exports = fibonacci;
