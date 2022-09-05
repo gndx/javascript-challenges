8b5d9ce8d4866b43472da874e097102702715b8c
Gerardo Manuel Chavez Larios <gerardo.larios69@gmail.com>
2022-09-05 12:12:36 -0500

    add trialDivision function

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,24 @@
 const trialDivision = (number) => {
-  // your code here
+  if (typeof number === 'number' && !Number.isInteger(number)) {
+    return false; 
+  }
+
+  if (number <= 1) {
+    return false; 
+  }
+
+  if (number % 2 === 0 && number > 2) {
+    return false; 
+  }
+
+  const sqrt = Math.sqrt(number);
+  for (let i = 3; i <= sqrt; i += 2) {
+    if (number % i === 0) {
+      return false; 
+    }
+  }
+
+  return true;
 }
 
 module.exports = trialDivision;
