1eca222f84a61c7688da63984a6d1e4de5c4912d
RedyRI <redy.aries12@gmail.com>
2022-09-05 11:11:15 -0500

    reto terminado

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,17 @@
 const trialDivision = (number) => {
-  // your code here
+  if (Number.isInteger(number)) {
+    if (number <= 1) {
+      return false;
+    } else {
+      for (let i = 2; i < number; i++) {
+        if (number % i == 0) {
+          return false;
+        }
+      }
+      return true;
+    }
+  }
+  return false;
 }
 
 module.exports = trialDivision;
