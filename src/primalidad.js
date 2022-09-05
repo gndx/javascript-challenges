25ac2b5ed884949ea723fee47daae4c431a194b9
MarcoAdame <marcoadame45@gmail.com>
2022-09-05 13:29:40 -0600

    Problems Solved

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,19 @@
-const trialDivision = (number) => {
+const trialDivision= (number) => {
   // your code here
+
+  var div, result;
+  
+  result = true;
+
+  for(div=2;div<(number/2);div++){
+    if(number%div==0) //is divider
+    {
+      result=false;
+      break; 
+    }
+  }
+  return result;
+
 }
 
 module.exports = trialDivision;
