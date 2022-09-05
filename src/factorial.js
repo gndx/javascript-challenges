3637a55e17fe45358cb616dc65a5718a0e5d7952
Ronald A. Vera C <104861423+ecuadorian-son@users.noreply.github.com>
2022-09-05 10:58:10 -0500

    Update factorial.js
    resolved

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,10 @@
 const factorial = (number) => {
-  // your code here
+  let result = number;
+  while number != 1 {
+    number -= 1
+    resulr *= factorial(number)
+  }
+  return result
 }
 
 module.exports = factorial;
