5a1c105d004c07e7f6d46ebbd83bd1cbb82049d7
Alvaro Torres <a.torresreyes@dxc.com>
2022-09-05 10:44:48 -0500

    feat(): prueba fibonacci+

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,20 @@
 const fibonacci = (n) => {
-  // your code here
+  if (typeof n != 'number') {
+    return 'numero no valido'
+  }
+  let n1 = 0;
+  let n2 = 1;
+  let nSiguiente = 0;
+  let arrayReturn = [0,1];
+
+  for (let x = 1; x<= n; x ++  ) {
+    nSiguiente = n1+n2;
+    arrayReturn.push(nSiguiente);
+    n1 = n2;
+    n2 =  nSiguiente;
+  }
+  const arrayR = arrayReturn.slice(0,n)
+  return arrayR;
 }
 
 module.exports = fibonacci;
