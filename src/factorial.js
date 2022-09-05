b07debd5a352b7e5f8c41251684765f40a82e1d9
federicorojas11 <federico.rojas.121@gmail.com>
2022-09-05 12:24:13 -0300

    factorial update

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,16 @@
 const factorial = (number) => {
-  // your code here
-}
+  let result = 0;
+
+  // number 0 and 1 cases
+  if (number === 0 || number === 1) {
+    result = 1;
+  } else {
+    for (let index = number; index !== 0; index--) {
+      // check if first number is assigned first
+      result === 0 ? (result = index) : (result *= index);
+    }
+  }
+  return result;
+};
 
 module.exports = factorial;
