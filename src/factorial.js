ee1973f4317007d89931bf715479877f6dabc691
Juan Antonio <srjuan54@gmail.com>
2022-09-05 12:46:56 -0500

    feat: reto cumplido Juan Antonio Chiclayo Peru

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,17 @@
 const factorial = (number) => {
-  // your code here
+  arr = [];
+  operacion = 1;
+  if(number > 0){
+    for (i= 1; i <= number; i++){
+      arr.push(i)
+    }
+    for (i= 0; i < number; i++){
+      operacion = operacion * arr[i];
+    }
+    return operacion;
+  }
+  else
+    return 1;
 }
 
 module.exports = factorial;
--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = (n) => {
-  // your code here
+const fibonacci = (number) => {
+  const result = [];
+
+  if(number > 0){
+      var a = 0;
+      var b = 1;
+      var ope = 0;
+
+    for (var i= 0; i < number; i++){
+      if(i == 0){
+          result.push(b)
+      }
+      else{
+          ope = a + b;
+          result.push(ope)
+          a = b;
+          b = ope;
+      }
+    }
+    return result;
+  }
+  else
+    return 0;
 }
 
 module.exports = fibonacci;
