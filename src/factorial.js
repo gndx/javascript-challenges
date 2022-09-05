6571e200cee056986eb6f49b3215fc3e8d1363a7
Diego Andrés Torres Riveros <88874892+diegotorres3006@users.noreply.github.com>
2022-09-05 12:15:40 -0500

    Ejercicio 1

--- src/factorial.js
+++ src/factorial.js
@@ -1,10 +1,8 @@
 const factorial = (number) => {
 	var total = 1; 
-	for (i=1; i<=number; i++) 
-	    {
+	for (i=1; i<=number; i++) {
 		total = total * i; 
     	}
 	return total; 
 }
-
 module.exports = factorial;
