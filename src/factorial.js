f2dd892604a13560366690a7d530642cb8116d4c
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2022-09-05 15:01:20 -0500

    [add] Challegens resolved

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,9 @@
 const factorial = (number) => {
-  // your code here
+  let arr = [], operacion = 1;
+  if(number < 0) return 1;
+  for (i= 1; i <= number; i++){ arr.push(i); }
+  for (i= 0; i < number; i++){ operacion = operacion * arr[i]; }
+  return operacion;
 }
 
 module.exports = factorial;
--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,11 @@
 const trialDivision = (number) => {
-  // your code here
+  let divisible = 0, i;
+
+  for (i = 2; i <= number; i++) {
+      if (number % i == 0){ divisible +=1; }
+  };
+
+  return divisible == 1; 
 }
 
 module.exports = trialDivision;
