b6ed3c0e612154ab75f59da801a0a09400744c0d
yohanaFalconi <yohanafalconivivas@gmail.com>
2022-09-05 15:22:21 -0500

    platzi js challeges resolved

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,10 +1,10 @@
 const fibonacci = (n) => {
-  let initial = [1, 1];
+  let init = [1, 1];
   let result = [1, 1];
 
   for (let i = 2; i <= n; i++) {
-    initial[i] = initial[i - 1] + initial[i - 2];
-    result.push(initial[i]);
+    init[i] = init[i - 1] + init[i - 2];
+    result.push(init[i]);
   }
 
   result.length = result.length - 1;
