52824b10cde0753584ad7271764f0842f5b569a2
Nicolas_Martinez <nicolas9seis@gmail.com>
2022-09-05 16:26:05 -0500

    Solutions of the challenges

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,15 @@
 const factorial = (number) => {
-  // your code here
+  //Check if the number is positive 
+  if(number > 0){
+    for(i = 1; i <= number; i++){
+      var result = i*i;
+    }
+    console.log("The factorial of " + number + " is " + result );
+    return result;
+  }else{
+    console.log("Number is negative");
+  }
+
 }
 
 module.exports = factorial;
