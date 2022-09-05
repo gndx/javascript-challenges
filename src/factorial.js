ba9e56581b85e315c19eba81d36bfc91a719eb8a
PabloSA57 <camusps577@gmail.com>
2022-09-05 17:35:49 -0300

    ejercicios completo

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,11 @@
 const factorial = (number) => {
   // your code here
+  if(number === 0) return 1
+  let multiplicacion = number
+  for(let i =number - 1 ; i >= 1; i--){
+    multiplicacion = multiplicacion * i 
+  }
+  return multiplicacion
 }
 
 module.exports = factorial;
