b128a3381c17c99cf34c71cf9bde57ae3a9ca649
Mike071-4810 <reyeshernandezm071@gmail.com>
2022-09-05 17:29:14 -0400

    Creada la funcion de primalidad

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,17 @@
-const trialDivision = (number) => {
-  // your code here
+const trialDivision = (number ) => {
+  
+  if(number<2)
+    return false;
+
+  if(!Number.isInteger(number))
+    return false;
+
+  for(let i = 2; i <= Math.sqrt(number); i++)
+    if( number % i === 0)
+      return false;
+  
+
+  return true;
 }
 
 module.exports = trialDivision;
