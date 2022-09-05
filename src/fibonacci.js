ce1795661dc3418813771da41e1b63d13e8ee717
Mayder C <maydercortes1@gmail.com>
2022-09-05 09:46:58 -0600

    solutions

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,11 @@
 const fibonacci = (n) => {
-  // your code here
+  if(n == 1) return [1]
+  const arr =  [1, 1]
+  for(let i =2; i < n; i ++){
+    arr[i] = arr[i-1] + arr[i-2] 
+
+  }
+  return arr
 }
 
 module.exports = fibonacci;
