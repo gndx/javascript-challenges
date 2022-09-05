f99dff53c039b7ed801ae50e5da1abe825b99b28
Yolvi Rosas <yolvi.rosasmoreno@gmail.com>
2022-09-05 13:40:04 -0500

    Adding solution

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,21 @@
 const fibonacci = (n) => {
   // your code here
+  let fb = [1,1]
+  let a = 0;
+  if (n>=2){
+    while (n > fb.length){
+
+      a = fb[fb.length-1] + fb[fb.length-2]
+      fb.push(a)
+    }
+    return fb
+  }
+  else{
+    fb = [1]
+    return fb
+  }
+
 }
 
+// console.log(fibonacci(1))
 module.exports = fibonacci;
