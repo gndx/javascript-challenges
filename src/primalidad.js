6c21669f40be99f3e29d93fa81abf3bc6d997052
deoraczo <deoraczo@gmail.com>
2022-09-05 11:02:47 -0500

    feat: primalidad

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,15 @@
 const trialDivision = (number) => {
-  // your code here
+  if (!Number.isInteger(number) || number <= 1) {
+    return false;
+  }
+
+  for (let i = 2; i <= number / 2; i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 module.exports = trialDivision;
