a5ddfe6ff7984b934bbb0da5531ebb02972bf154
Leugim25 <miguel25ibanez@gmail.com>
2022-09-05 12:30:30 -0500

    Solución de los 3 retos con js

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,13 @@
 const factorial = (number) => {
-  // your code here
+  if(number != Math.abs(number)){
+    console.log("Ingrese valores enteros positivos")
+  }else{
+    let n = number;
+    for (let i = 1; i < number; i++) {
+      n = n*i;
+    }
+    return n;
+  }
 }
 
-module.exports = factorial;
+factorial(5);
