0707503be033d7bc2bc07bdccaca6a7f6d6dfc93
veronicajujuy <veronicajujuy@gmail.com>
2022-09-05 16:53:49 -0300

    pasando tests

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,14 @@
 const trialDivision = (number) => {
   // your code here
+  if(number < 1) return false
+  if (number % 1 != 0) return false
+  if (number == 0 || number == 1 || number == 4) return false
+  for (i = 2; i < number / 2; i++) {
+    if (number % i == 0) return false
+  }
+  return true;
+
 }
 
 module.exports = trialDivision;
+
