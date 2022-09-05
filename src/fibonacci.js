54db7eac0c4754c5aa5836fec0e6c5230a2ba260
MrMarin7890 <sergiosrt12@gmail.com>
2022-09-05 15:41:53 -0500

    Retos resueltos

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,11 @@
 const fibonacci = (n) => {
   // your code here
+  tnt = [0,1];
+  for (i=2;i<=n; i++){
+    tnt[i]= tnt[i-1] + tnt[i-2];
+  }
+  tnt.shift()
+  return tnt;
 }
 
 module.exports = fibonacci;
--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,12 @@
-const trialDivision = (number) => {
+const trialDivision = (num) => {
   // your code here
-}
+  if (!Number.isInteger(num))return false 
+
+  for (i= 2; i<num; i++){
+    if (num % i == 0) return false
+  }
+  return num>1
+};
+
 
 module.exports = trialDivision;
