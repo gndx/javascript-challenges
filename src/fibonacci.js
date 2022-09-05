8be34e64e05ea021578dd6d3fb820fa3476d4fba
Ricardo Rivera <ricardo.rivera@indava.com>
2022-09-05 10:57:01 -0500

    solve all

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,23 @@
 const fibonacci = (n) => {
-  // your code here
+  let res = [1,1];
+  let a = 1, b = 1;
+  let memory;
+
+  if (n === 1) {
+      return [1];
+  }
+
+  if (n === 2) {
+      return [1,1];
+  }
+
+  for (let i = 2 ; i < n ; i++) {
+      memory = a+b;
+      res.push(memory);
+      a = b;
+      b = memory;
+  }
+  return res;
 }
 
 module.exports = fibonacci;
--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,14 @@
 const trialDivision = (number) => {
-  // your code here
+    if (number <= 1 || number%1 !== 0) {
+        return false;
+    }
+    for (let i = 2 ; i < number ; i++) {
+        if ( number%i === 0) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 module.exports = trialDivision;
