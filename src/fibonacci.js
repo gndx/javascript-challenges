f654684fb22504a7c0b0cf930f7524aeb58c27b2
JuanCarlosLP <jclarios.js@gmail.com>
2022-09-05 11:25:24 -0500

    Javascript Challenge

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,15 @@
 const fibonacci = (n) => {
-  // your code here
+  let number1 = 1;
+  let number2 = 1;
+  let next = 0;
+  let fibonacciResult = [];
+  for (let x = 0; x < n; x++) {
+    fibonacciResult.push(number1);
+    next = number1 + number2;
+    number1 = number2;
+    number2 = next;
+  }
+  return fibonacciResult;
 }
 
 module.exports = fibonacci;
