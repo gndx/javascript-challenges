0b9c538cb5f6bef28e5738a5e3dd91f213248db2
leslye <leslyellata.97@hotmail.com>
2022-09-05 15:37:26 -0300

    Adding fibonacci solution

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,16 @@
 const fibonacci = (n) => {
-  // your code here
+  let fibonacci_series = [];
+
+  for (let i = 0; i < n; i++) {
+    if (i>=2){
+      fibonacci_series[i]= fibonacci_series[i-1] + fibonacci_series[i - 2];
+    }
+    else {
+      fibonacci_series[i]=1;
+    }
+  }
+  return fibonacci_series;
 }
+
 
 module.exports = fibonacci;
