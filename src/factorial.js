4dce2e0085bf0bce2713a54a40243d678a557eb3
lpfalcon <lpfalcon99@gmail.com>
2022-09-05 18:22:25 -0500

    Platzi Challenge

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,11 @@
 const factorial = (number) => {
   // your code here
+  if (number <= 1) {
+    return 1
+  } else {
+    return number * factorial(number - 1)
+  }
 }
+
 
 module.exports = factorial;
