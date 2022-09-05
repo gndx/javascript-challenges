b8a8094ac058786739f22d77d6a0186bd7fe4d2c
japsolo <jh.docente@gmail.com>
2022-09-05 10:50:57 -0500

    solved

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,11 @@
 const fibonacci = (n) => {
-  // your code here
+  if (!n || isNaN(n)) throw Error("Please, provide a number");
+  const secuence = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    secuence[i] = secuence[i - 2] + secuence[i - 1];
+  }
+  secuence.shift();
+  return secuence;
 }
 
 module.exports = fibonacci;
