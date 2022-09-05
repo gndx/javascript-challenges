cc0653ba9f835149d62cfb9cfc34a450c263f78b
Julian Montufar <julamontufar@misena.edu.co>
2022-09-05 12:21:46 -0500

    Challenge solved all

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,16 @@
 const fibonacci = (n) => {
   // your code here
-}
+  let num1 = 1;
+  let num2 = 1;
+  let con = 0;
+  let fibo = []
+  for (let i = 0; i < n; i++) {
+    fibo.push(num1)
+    con = num1 + num2;
+    num1 = num2;
+    num2 = con;
+  }
+  return fibo;
+};
 
 module.exports = fibonacci;
