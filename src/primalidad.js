f908d9e25ace365f91bedafd9e47af696f2ffd84
Antonio Soto <fantoniosoto@gmail.com>
2022-09-05 12:54:54 -0500

    challenges solved

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,10 @@
 const trialDivision = (number) => {
-  // your code here
+  if (!Number.isInteger(number) || number <= 0) return false
+  if (number === 1) return false
+  if (number === 2) return true
+  const limit = Math.sqrt(number) >> 0 
+  for (let i = 2; i <= limit; i++)  if (number % i === 0) return false
+  return true
 }
 
 module.exports = trialDivision;
