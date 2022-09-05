b740a2401e0210645e2698f83fb498a85a3faa39
Juan Gomez <juanjose@ingomez.com>
2022-09-05 12:15:37 -0500

    Problemas resueltos.

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,17 @@
 const fibonacci = (n) => {
   // your code here
+  let data = [];
+
+  if (n === 1) {
+    data = [n];
+  } else {
+    data = [1, 1];
+    for (var i = 2; i < n; i++) {
+      data.push(data[i-2] + data[i-1]);
+    }
+  }
+
+  return data;
 }
 
 module.exports = fibonacci;
