330f2a92b142bbb711d5888065b708382e45b52a
INGENIERO3 <INGENIERO3@pyt.local>
2022-09-05 14:59:30 -0500

    [ADD] resolucion de los retos

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,11 @@
-const factorial = (number) => {
-  // your code here
-}
+// const factorial = (number) => {
+//   // your code here
+//   let valor = number
+//   for( let i = number-1; i > 0; i--  ) {
+//     let multiplicacion = valor * i;
+//     valor = multiplicacion;
+//   }
+// }
 
-module.exports = factorial;
+
+//  module.exports = factorial;
--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,14 @@
 const trialDivision = (number) => {
   // your code here
+  let conteo = 0;
+  for( let i = 1; i <= number; i++ ) {
+    if( number%i == 0){
+      conteo = conteo + 1;
+    }
+    
+  }
+  conteo > 2 ? console.log( 'El numero no es primo' ) : console.log( 'El numero es primo' );
+ 
 }
 
 module.exports = trialDivision;
