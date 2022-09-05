969b3081a313bf058c79113dacd1fcb90c13883e
federicorojas11 <federico.rojas.121@gmail.com>
2022-09-05 12:45:55 -0300

    primal update

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,18 @@
 const trialDivision = (number) => {
-  // your code here
-}
+  // first exclude negative, numbers less than two,
+  // and decimal numbers
+  if (number < 2 || number % 1 != 0) {
+    return false;
+  }
+
+  for (let i = 0; i <= number; i++) {
+    for (let j = 0; j <= number; j++) {
+      if (i * j === number && i !== number && j !== number) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
 
 module.exports = trialDivision;
