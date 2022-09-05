00c610a79910d3309c5164828b725f7939546458
Jesus <jesusalfonsojg7@gmail.com>
2022-09-05 17:06:17 -0400

    Commit.

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,13 @@
 const fibonacci = (n) => {
   // your code here
+    if (n === 1) return [1]
+    let fib = [1, 1];
+
+    for (let i = 2; i < n; i++) {
+        fib.push(fib[i - 1] + fib[i - 2]);
+    }
+
+    return fib
 }
 
 module.exports = fibonacci;
