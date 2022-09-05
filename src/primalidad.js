4d7a85aeced45181bb63859b10ac7a3144df66af
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-09-05 11:37:55 -0400

    feat: resuelto challenge de primalidad

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,19 @@
 const trialDivision = (number) => {
-  // your code here
-}
+  // si no es numero entero o es igual a 1, entonces devuelve false
+  if (number <= 1 || !Number.isInteger(number)) {
+    return false;
+  }
+
+  let isPrime = true;
+  // busca cada posible divisor del numero, si encuentra alguno entonces
+  // no es primo
+  for (let i = number - 1; i > 1; i--) {
+    if (number % i === 0) {
+      isPrime = false;
+    }
+  }
+
+  return isPrime;
+};
 
 module.exports = trialDivision;
