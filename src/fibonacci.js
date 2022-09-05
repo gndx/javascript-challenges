7ce67b66e7c1a8585063986cc2373edde9f20dac
diegocarlosdiaz <diegocarlosdiaz5@gmail.com>
2022-09-05 16:57:32 -0300

    arreglos

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,11 +1,11 @@
 const fibonacci = (n) => {
-  const basic = [0, 1];
+  const basic = [0,1];
   for(let i = 2; i <= n; i++) {
     basic.push(basic[i-1] + basic[i - 2]);
   }
-  return basic;
+  return basic.splice(1,n);
 }
 
-console.log(fibonacci(20));
-
+/* console.log(fibonacci(2));
+ */
 module.exports = fibonacci;
--- src/primalidad.js
+++ src/primalidad.js
@@ -1,16 +1,4 @@
 const trialDivision = (n) => {
-  if (typeof n != 'number') {
-    throw TypeError('El argumento debe ser un número.');
-}
-
-if (!Number.isInteger(n)) {
-    throw TypeError('El argumento debe ser un número entero.');
-}
-
-if (n <= 1) {
-    throw Error('El argumento debe ser un número entero positivo.');
-}
-
 let raiz = Math.floor(Math.sqrt(n)) + 1;
 
 for(let i = 2; i < raiz; ++i) {
