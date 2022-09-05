29834ef107cad7d78db9e2a73703e9cd9812ae4c
Jesus Rodriguez <mc.ireiser@gmail.com>
2022-09-05 13:59:53 -0400

    Factorial

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,15 @@
 const factorial = (number) => {
-  // your code here
-}
+  if (number === 0 || number === 1) return 1;
+
+  let total = number;
+
+  for (let i = number; i > 0; i--) {
+    if (i < number) {
+      total = i * total;
+    }
+  }
+
+  return total;
+};
 
 module.exports = factorial;
