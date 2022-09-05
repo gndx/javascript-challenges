57435d9a90b8e4b8a88a7b1d6c7839aea14e8cb1
camilogonzalez543 <88495572+camilogonzalez543@users.noreply.github.com>
2022-09-05 16:02:50 -0500

    Update primalidad.js

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,9 @@
 const trialDivision = (number) => {
-  // your code here
+ if (number < 0 || !Number.isInteger(number)) return false;
+  if(number === 0 || number === 1 || number === 4) return false;
+  for (let index = 2; index < number/2; index++) {
+    if (number % index === 0) return false;
+  }
+  return true;
 }
-
 module.exports = trialDivision;
