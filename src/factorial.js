aa87e939dcbacbdffc44ab5846a3d6474e2e97c3
Yunior Cabrera <yvcabrerago@gmail.com>
2022-09-05 17:47:49 +0200

    Retos completados

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,9 @@
 const factorial = (number) => {
   // your code here
+  if (number === 0 || number === 1)
+    return 1;
+  else
+    return number * factorial(number - 1);
 }
 
 module.exports = factorial;
--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,12 @@
 const trialDivision = (number) => {
   // your code here
+  if (number <= 1) return false;
+  if (!Number.isInteger(number)) return false;
+  for (let i = 2; i <= Math.round(Math.sqrt(number)); i++) {
+    if (number % i === 0)
+      return false;
+  }
+  return true;
 }
 
 module.exports = trialDivision;
