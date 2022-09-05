3fbc696fb2ec15b3dd4736b542ff651536bc7319
Mike071-4810 <reyeshernandezm071@gmail.com>
2022-09-05 17:14:55 -0400

    Creada funcion del fibonacci

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,25 @@
 const fibonacci = (n) => {
-  // your code here
+
+  if(n <= 0)
+  throw new Error("Fibonacci no permitido");
+
+  if(n === 1)
+    return [1];
+
+  let actualFibonacci = n_fibonacci(n);
+
+  return [...fibonacci(n-1), actualFibonacci];
+}
+
+const n_fibonacci = (n) => {
+  
+  if(n <= 0)
+  throw new Error("N-esimo fibonacci no permitido");
+
+  if(n === 1 || n === 2 )
+  return 1;
+
+  return n_fibonacci(n-1) + n_fibonacci(n-2);
 }
 
 module.exports = fibonacci;
