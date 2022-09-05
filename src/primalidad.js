f4f73d919b0ddb4fdc403e9f982d90a74dbd0423
Jesus Cuesta <jesus@jesuscuesta.net>
2022-09-05 20:00:17 +0200

    feat(resolve): resuelto los tres ejercicios

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,12 @@
 const trialDivision = (number) => {
-  // your code here
+  number = Math.abs(number);
+  // Casos especiales
+	if (number == 0 || number == 1 || number == 4 || number % 1 != 0) return false;
+	for (let x = 2; x < number / 2; x++) {
+		if (number % x == 0) return false;
+	}
+	// Si no se pudo dividir por ninguno de los de arriba, sí es primo
+	return true;
 }
 
 module.exports = trialDivision;
