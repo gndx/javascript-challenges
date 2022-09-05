1ca880b8c1ef92712c0d374eade9a2d8979008d3
Avocado <43632131+Awoocado@users.noreply.github.com>
2022-09-05 15:34:53 +0000

    Resolviendo los retos

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,17 @@
 const factorial = (number) => {
-  // your code here
+  if (number == 0 || number == 1) return 1
+  return factorial(number - 1) * number
 }
 
-module.exports = factorial;
+// Como solución alternativa se puede usar un array para evitar calcular el factorial de un número más de una vez.
+/*
+const arr = []
+
+const factorial = (number) => {
+  if (number == 0 || number == 1) return 1
+  if (arr[number] > 0) return arr[n]
+  return arr[number] = factorial(number - 1) * number
+}
+*/
+
+module.exports = factorial
--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,9 @@
 const fibonacci = (n) => {
-  // your code here
+  const arr = []
+  for (let i = 0; i < n; i++) {
+    arr.push(i < 2 ? 1 : arr[i - 1] + arr[i - 2])
+  }
+  return arr
 }
 
-module.exports = fibonacci;
+module.exports = fibonacci
