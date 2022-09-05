850140e9fb413c26476fba47615c0a53fdde77f0
Ricardo Alfredo Robledo <rrobledo@quanto.mx>
2022-09-05 12:31:33 -0600

    Correcciones

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -3,6 +3,7 @@ const fibonacci = (n) => {
   for(i=2; i<=n; i++){
     fiboNumbers.push(fiboNumbers[i-1]+fiboNumbers[i-2])
   }
+  fiboNumbers.splice(0,1)
   return fiboNumbers
 }
 
--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,5 @@
 const trialDivision = (number) => {
-  if (number == 4 || number <=1) return false;
+  if (number == 4 || number <=1 || !Number.isInteger(number)) return false;
 	for (i = 2; i < number / 2; i++) {
 		if (number % i == 0) return false;
 	}
