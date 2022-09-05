49db0d147c0a4f05795be5a8e2cccdb699efd5f8
Christopher Jesús <92419683+christo3carrasco@users.noreply.github.com>
2022-09-05 12:45:04 -0500

    Solved

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,10 @@
 const fibonacci = (n) => {
-  // your code here
+  const arr = [0, 1];
+  for (let i = 1; i <= n; i++) {
+    const x = arr[i] + arr[i - 1];
+    arr.push(x);
+  }
+  return arr;
 }
 
 module.exports = fibonacci;
--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,8 @@
 const trialDivision = (number) => {
-  // your code here
+  for (let i = 2; i < number; i++) {
+    if (number % i === 0) return false;
+  }
+  return n > 1;
 }
 
 module.exports = trialDivision;
