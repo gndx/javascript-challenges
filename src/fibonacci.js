dcf14a362068bc17545341e4ce692242a1587b53
Ronald <ronaldherpc@gmail.com>
2022-09-05 12:15:25 -0400

    Soy estudiante de Ing. Informatica y tambien trabajo, el ganar la beca y conseguir un mejor me ayudaria tanto a mi como a mi familia

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,21 @@
 const fibonacci = (n) => {
-  // your code here
+
+  if (n == 1) return [1];
+  else if (n == 2) return [1, 1];
+
+  let a = 1, b = 1, c;
+  let arr = [1, 1];
+
+
+  for (let i = 0; i < n - 2; i++)
+  {
+    c = a + b;
+    arr.push(c);
+    a = b;
+    b = c;
+  }
+
+  return arr;
 }
 
 module.exports = fibonacci;
