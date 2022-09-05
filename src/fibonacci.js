24e9d8e014f1b6d080b6437ef529562732dfe818
Ronald A. Vera C <104861423+ecuadorian-son@users.noreply.github.com>
2022-09-05 13:32:22 -0500

    Update fibonacci.js

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,12 +1,12 @@
 const fibonacci = (n) => {
-    let result = [1];
+    let result = [1, ];
     let current_number = 1;
     while (true){
         result_len = result.length
         if (result_len >= 2){
             current_number = result[result_len-2] + result[result_len-1];
         }
-        if (current_number < n){
+        if (current_number <= n){
             result.push(current_number);
         }else{
             break;
