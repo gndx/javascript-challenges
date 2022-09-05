b8f87ae56bb80c9896c00d8253441953b933f28d
JLCareglio <j.l.careglio.albornoz@gmail.com>
2022-09-05 17:43:34 -0300

    ✅ test superado 💪

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,7 @@
 const trialDivision = (number) => {
   // your code here
-}
-
+  if (number <= 1 || number == 4 || !Number.isInteger(number)) return false;
+  for (let i = 2; i < number / 2; i++) if (number % i == 0) return false;
+  return true;
+};
 module.exports = trialDivision;
