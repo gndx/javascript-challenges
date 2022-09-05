2705d554e1a056035fb5a38d394903b7cf09227a
William Arevalo <willarevalo.developer@gmail.com>
2022-09-05 12:28:13 -0500

    Completed challenge

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,10 @@
 const factorial = (number) => {
-  // your code here
+  // for loop for mul the total.
+  var total = 1; 
+	for (i=1; i<=number; i++) {
+		total *= i; 
+	}
+	return total;
 }
 
 module.exports = factorial;
