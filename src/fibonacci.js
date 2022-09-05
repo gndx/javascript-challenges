880b3200c17d0b53ae218d17d658ad890d97bd8e
Alex Herrera <38311783+AlexHerrerax@users.noreply.github.com>
2022-09-05 12:24:23 -0300

    Ejercicios resueltos

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,19 @@
 const fibonacci = (n) => {
-  // your code here
-}
+  if(n === 1) return [1]
+   let inicio=1
+   let final =1;
+   let todos = [inicio, final]
+ 
+   for(let i=1; i<n-1; i++){
+    let nuevo = inicio + final;
+     todos.push(nuevo);
+     inicio = final;
+     final = nuevo;
+   }
+ return todos;
+ }
+
+
+//console.log(fibonacci(8))
 
 module.exports = fibonacci;
--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,12 @@
 const trialDivision = (number) => {
-  // your code here
+  if(number <=1 || !Number.isInteger(number)) return false;
+  let div = 0;
+  for(let i=number-1; i>=2; i--){ 
+    if(number % i ===0){
+      console.log(i, "Es divisible")
+      div +=1;
+    }}
+return div>=1 ? false: true
 }
 
 module.exports = trialDivision;
