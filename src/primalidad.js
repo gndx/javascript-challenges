aabeaa93f0756e5569d18030715a1b766b77c592
Juan Segundo Riera <rierajuanse@gmail.com>
2022-09-05 13:30:44 -0300

    Solved test

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,16 @@
 const trialDivision = (number) => {
-  // your code here
-}
+	// your code here
+	if (!Number.isInteger(number) || number <= 1) {
+		return false;
+	}
+	let isPrime = true;
+	for (let i = 2; i < parseInt(Math.sqrt(number)) + 1; i++) {
+		if (number % i == 0) {
+			isPrime = false;
+			break;
+		}
+	}
+	return isPrime;
+};
 
 module.exports = trialDivision;
