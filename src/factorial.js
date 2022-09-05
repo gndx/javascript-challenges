32f79fc534493edc4840aa87341a487655b102d1
tomivm <tomivm98@gmail.com>
2022-09-05 15:44:51 -0300

    resolved :)

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,13 @@
 const factorial = (number) => {
   // your code here
-}
+  const multiply = (actualValue, multipliedBy = number) => {
+    if (multipliedBy === 1) return actualValue;
+    const multyplyBy = multipliedBy - 1;
+    const result = actualValue * multyplyBy;
+    return multiply(result, multyplyBy);
+  };
+  if (number === 0) return 1;
+  return multiply(number);
+};
 
 module.exports = factorial;
