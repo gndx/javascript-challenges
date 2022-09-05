b92000a8ccec5d67cda2aed122916e6361f6edb3
Diego Munoz <dimuva_25@hotmail.com>
2022-09-05 12:06:50 -0500

    envío reto

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,10 @@
 const fibonacci = (n) => {
   // your code here
+  var arr = [0, 1];
+  for (var i = 2; i < n + 1; i++){
+    arr.push(arr[i - 2] + arr[i -1])
+  }
+ let arrOutZero = arr.shift()
+ return arr
 }
-
 module.exports = fibonacci;
