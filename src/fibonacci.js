0e572a9a67ba8f860556abdb1e9b3c06d5a1d10f
Ivan Medina <ivan.medina@yalo.com>
2022-09-05 14:55:45 -0300

    add solutions

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,9 @@
 const fibonacci = (n) => {
-  // your code here
+  let result = [1]
+  for (let i = 1; i < n; i++) {
+    result.push(result[i - 1] + (result[i - 2] ?? 0))
+  }
+  return result
 }
 
 module.exports = fibonacci;
