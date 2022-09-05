37fa83b0125ffac21dc5cab1acd157c1e4c75919
unknown <guidomgm@gmail.com.com>
2022-09-05 14:48:06 -0500

    aporte de Guido Gutierrez

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,12 @@
 const fibonacci = (n) => {
-  // your code here
+  sol = [0, 1];
+  //if (n <= 1) return sol[1];
+  for (let i = 2; i<= n; i++) {
+      sol[i] = sol[i -1] + sol[i - 2];
+    
+  }
+  sol.shift()
+  return sol ;
 }
 
 module.exports = fibonacci;
