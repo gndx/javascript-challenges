2a57342ecd420fd307f89dd4dc2bf1e097bcf23b
Johannes Gómez <87533958+JohannesGomez@users.noreply.github.com>
2022-09-05 19:27:43 -0300

    Challenger finished successfully

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,9 @@
 const trialDivision = (number) => {
   // your code here
+    if(number<=1 || !Number.isInteger(number))  return false;
+      for (let index = 2; index <= number-1; index++) 
+          if(number%index === 0) return false
+    return true;   
 }
 
 module.exports = trialDivision;
