3e94a9ea42ce5525b78e44f931ffccd627fccfd4
GaryCraft <garycraft@our-space.xyz>
2022-09-05 10:50:00 -0500

    Solved Challenges

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,13 @@
 const factorial = (number) => {
-  // your code here
+  if (number < 0) {
+	return -1;
+  }
+  else if (number == 0) {
+	return 1;
+  }
+  else {
+	return (number * factorial(number - 1));
+  }
 }
 
 module.exports = factorial;
