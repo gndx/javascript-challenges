23bdc160d996a8be15361a8a91a464517a32ac96
stbedoyaa <91540848+stbedoyaa@users.noreply.github.com>
2022-09-05 16:08:08 -0500

    Update factorial.js

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,8 @@
 const factorial = (number) => {
-  // your code here
+  let valor = number;
+  for( let i = number-1; i > 0; i--  ) {
+    let multiplicacion = valor * i;
+    valor = multiplicacion;
+  }
 }
-
 module.exports = factorial;
