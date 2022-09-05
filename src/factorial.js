686b9fa2b82de958f8aee1d1a9ab286dff135995
BrayanBaquero <73759467+BrayanBaquero@users.noreply.github.com>
2022-09-05 12:17:10 -0500

    challenges

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,10 @@
 const factorial = (number) => {
   // your code here
+  let output=1;
+  for (let i=1; i<=number;i++){
+    output=output*i;
+  }
+  return output;
 }
 
 module.exports = factorial;
