b6187b14a255d7cee7940b8bf5b24d9881e49a2f
paulacarvani <3312@holbertonschool.com>
2022-09-05 12:33:46 -0500

    ADD: function to return an fibonacci array, all test passed

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,15 @@
 const fibonacci = (n) => {
   // your code here
+  fq = [1, 1]
+  if (n <= 1){return [ (fq[1]) ] }
+  if (n == 2){return (fq)}
+
+  if (n > 2){
+    for (let i = 1; i <= (n-2); i++) {
+      fq.push(fq[fq.length-1] +  fq[fq.length-2])
+    }
+    return(fq)
+  }
 }
 
 module.exports = fibonacci;
