c1b8d56915d09c6eb4582f12f5e8301060cde9c3
Yuniel Acosta Pérez <yunielacosta738@gmail.com>
2022-09-05 14:35:54 -0300

    fibonacci problem resolved

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,14 @@
 const fibonacci = (n) => {
-  // your code here
+    if (n === 1) {
+        return [1];
+    } else if (n === 2) {
+        return [1, 1];
+    }
+    const result = [1, 1];
+    for (let i = 2; i < n; i++) {
+        result.push(result[i - 1] + result[i - 2]);
+    }
+    return result;
 }
 
 module.exports = fibonacci;
