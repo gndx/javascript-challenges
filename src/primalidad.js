7e998d320eaae5c6228b12dbdcc93fd7fc7fa837
Nicolas_Martinez <nicolas9seis@gmail.com>
2022-09-05 16:32:25 -0500

    git

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,27 +1,28 @@
 const trialDivision = (number) => {
 
-    const number = parseInt(prompt("Enter a positive number: "));
-    let isPrime = true;
+  const num = parseInt(prompt("Enter a positive number: "));
+  let isPrime = true;
 
-    if (number === 1) {
-        console.log("1 is neither prime nor composite number.");
-    }else if (number > 1) {
+  if (num === 1) {
+      console.log("1 is neither prime nor composite number.");
+  }else if (num > 1) {
 
-        for (let i = 2; i < number; i++) {
-            if (number % i == 0) {
-                isPrime = false;
-                break;
-            }
-        }
+      for (let i = 2; i < num; i++) {
+          if (num % i == 0) {
+              isPrime = false;
+              break;
+          }
+      }
 
-        if (isPrime) {
-            console.log(`${number} is a prime number`);
-        }else {
-            console.log(`${number} is a not prime number`);
-        }
-    }else {
-        console.log("The number is not a prime number.");
-    }
+      if (isPrime) {
+          console.log(`${num} is a prime number`);
+      }else {
+          console.log(`${num} is a not prime number`);
+      }
+  }else {
+      console.log("The number is not a prime number.");
+  }
 }
+
 
 module.exports = trialDivision;
