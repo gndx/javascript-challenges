7953b144a9124e26cdc9392c7397b95a8919feed
RoyerManz <74071761+RoyerManz@users.noreply.github.com>
2022-09-05 17:59:59 -0400

    Update factorial.js

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,12 @@
 const factorial = (number) => {
   // your code here
+  let factorial = 1
+  let iterador = 1
+  while(iterador <= number){
+    factorial *= iterador
+    iterador ++
+  }
+  return factorial
 }
 
 module.exports = factorial;
