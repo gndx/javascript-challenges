9b6cd880b3885eb92a91a266d5c130512fb5703b
Antony Ventura Morey <aventura@indotel.gob.do>
2022-09-05 15:48:23 -0300

    Challenges DONE

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,11 @@
 const factorial = (number) => {
-  // your code here
+  if( number === 0 || number === 1 ) return 1;
+
+  let res = 1;
+  for(let i = number; i >= 1; i--) res = res * i;
+  
+  return res;
 }
+
 
 module.exports = factorial;
