68e49e708e5e9905e2511643bc319f4c61148f73
Luis G. Villegas <luisguveal@gmail.com>
2022-09-05 17:03:19 -0430

    Soluciones

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,12 @@
 const factorial = (number) => {
   // your code here
+
+  // condicion base
+  if (number <= 1) {
+    return 1;
+  }
+  // recursion
+  return number * factorial(number-1);
 }
 
 module.exports = factorial;
