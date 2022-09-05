ed7d49e7baad5922a5d8c855745d3872fef787ce
Jesus Rodriguez <mc.ireiser@gmail.com>
2022-09-05 14:00:24 -0400

    Fibonacci

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,16 @@
 const fibonacci = (n) => {
-  // your code here
-}
+  let total = [];
+
+  if (n === 1) return [1];
+
+  for (let i = 1; i < n; i++) {
+    if (total.length === 0) total.push(i);
+    const a = total.at(-2) ?? 0;
+    const b = total.at(-1) ?? 0;
+    total.push(a + b || i);
+  }
+
+  return total;
+};
 
 module.exports = fibonacci;
