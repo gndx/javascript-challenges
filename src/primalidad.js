dbadf03405d63124d5ed6eb7b497d3f11f86c071
Jesus Rodriguez <mc.ireiser@gmail.com>
2022-09-05 14:00:07 -0400

    Primalidad

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,18 @@
 const trialDivision = (number) => {
-  // your code here
-}
+  if (number < 2) return false;
+
+  let counter = 0;
+
+  for (let i = 2; i < number; i++) {
+    const result = number % i;
+    if (result !== 0) counter = counter + 1;
+  }
+
+  if (counter === number - 2) {
+    return number % number === 0 && number % 1 === 0;
+  }
+
+  return false;
+};
 
 module.exports = trialDivision;
