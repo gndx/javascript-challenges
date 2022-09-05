f7692e4e0c9f4d93ebb60c69a4f71064f8ed1514
diegocarlosdiaz <diegocarlosdiaz5@gmail.com>
2022-09-05 12:27:38 -0300

    ejercicio de factorial

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,12 @@
 const factorial = (number) => {
-  // your code here
+  if (n == 0) {
+    return 1;
+} else {
+    return n * factorial(n - 1);
 }
+}
+
+console.log(factorial(5));
+
 
 module.exports = factorial;
