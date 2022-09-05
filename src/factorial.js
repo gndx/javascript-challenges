3beab9c82a39279da29c7051fbacdb6f5570f716
ernestolopez887 <52922934+ernestolopez887@users.noreply.github.com>
2022-09-05 16:26:04 -0500

    Saludos!

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,11 @@
 const factorial = (number) => {
-  // your code here
+    if (number <= 1)
+        return 1;
+        
+    for (let ix = number - 1; ix > 1; ix--) 
+        number *= ix;
+        
+    return number;
 }
 
 module.exports = factorial;
