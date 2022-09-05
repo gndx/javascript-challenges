0e5d0e0e45bfd01252e10fe1c922af11b3adba41
Alan Álvarez <aran.712@gmail.com>
2022-09-05 14:10:55 -0500

    Challenge Finished

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,9 @@
 const factorial = (number) => {
-  // your code here
+  let fact = 1;
+  for(let i = 1; i<=number; i++){
+    fact = fact * i;
+  }
+  return fact;
 }
 
 module.exports = factorial;
--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,16 @@
+
 const fibonacci = (n) => {
-  // your code here
+  if (n == 1) {
+    return [1];
+  }else if (n == 2) {
+    return [1, 1]
+  } else{
+  const resultado = [1, 1]
+  for(let i = 2; i < n; i++){
+    resultado.push(resultado.slice(-1)[0] + resultado.slice(-2)[0])
+  }
+  return resultado
 }
+};
 
 module.exports = fibonacci;
