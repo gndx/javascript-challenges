67450c3c1cc2faf7c7cf3ed4ceb2b9600b73e2cf
Frederick Montiel <86737117+FrederickMontiel@users.noreply.github.com>
2022-09-05 10:10:53 -0600

    Soluciones

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,17 @@
 const fibonacci = (n) => {
-  // your code here
+  if(n > 1){
+    //Si lees esto, ¿por qué no tomas en cuenta el 0?
+    let result = [1, 1];
+    for (let i = 0; i < n-2; i++) {
+      result.push(result[result.length-1]+result[result.length-2]);
+    }
+
+  
+    return result;
+  }else{
+    return [1];
+  }
+
 }
 
 module.exports = fibonacci;
--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,21 @@
 const trialDivision = (number) => {
-  // your code here
+  if (number > 1 && Number.isInteger(number)) {
+    let coincidencias = 0;
+
+    for (let i = 2; i <= number; i++) {
+      if (number % i === 0) {
+        coincidencias++;
+      }
+    }
+
+    if (coincidencias > 1) {
+      return false;
+    }else{
+      return true;
+    }
+  }else{
+    return false;
+  }
 }
 
 module.exports = trialDivision;
