1466d99790ce282bf9981d6da9ed6000a38643fa
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-09-05 11:38:10 -0400

    feat: resuelto challenge de fibonacci

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,24 @@
 const fibonacci = (n) => {
-  // your code here
-}
+  let numbers = [];
+
+  // si n es igual a cero entonces devolvera un array vacio
+  while (n !== 0) {
+    // si el array numbers esta vacion entonces agrega 1.
+    if (numbers.length === 0) {
+      numbers.push(1);
+    } else {
+      // si ya tiene numeros entonces toma el ultimo y penultimo numnero
+      // de la lista y suma los dos numeros
+      // el resultado lo agrega al array numers
+      let last_number = numbers[numbers.length - 1] ?? 0;
+      let before_last = numbers[numbers.length - 2] ?? 0;
+
+      numbers.push(before_last + last_number);
+    }
+    n--;
+  }
+
+  return numbers;
+};
 
 module.exports = fibonacci;
