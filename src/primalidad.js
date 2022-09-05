262ad5089fdb0ad745c070c7e64406aa1a3d8bdc
Alvaro Torres <a.torresreyes@dxc.com>
2022-09-05 10:44:37 -0500

    feat(): prueba nprimo

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,13 @@
 const trialDivision = (number) => {
-  // your code here
-}
+  if (number <= 1 || typeof number != 'number') {
+    return 'no es valido';
+  }
+  for (let x = 2; x <= number - 1; x++) {
+    if (number % x == 0) {
+      return 'No es primo';
+    }
+  }
+  return 'Es Primo';
+};
 
 module.exports = trialDivision;
