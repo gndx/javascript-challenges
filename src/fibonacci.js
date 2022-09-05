15ef2e473f394490ea55689015e72657f355405c
Brandon Aray <alex.aray94@gmail.com>
2022-09-05 12:49:37 -0500

    retos terminados

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,16 @@
 const fibonacci = (n) => {
   // your code here
-}
+  const fibArr = [];
+
+  for (let i = 1; i <= n; i++) {
+    if (i <= 2) {
+      fibArr.push(1);
+    } else {
+      fibArr.push(fibArr[fibArr.length - 1] + fibArr[fibArr.length - 2]);
+    }
+  }
+
+  return fibArr;
+};
 
 module.exports = fibonacci;
