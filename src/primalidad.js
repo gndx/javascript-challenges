e611274d3bff88dac5a66a07e1938fd3577fcce0
Diego Andrés Torres Riveros <88874892+diegotorres3006@users.noreply.github.com>
2022-09-05 15:23:40 -0500

    Ejercicio 2

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,16 @@
 const trialDivision = (number) => {
-  // your code here
+    if (!Number.isInteger(number)){
+    return false;
+  }
+  if(number <= 1) {
+    return false;
+  }
+   for (var n = 2;  n<number;  n += 1) {
+    if(number % n == 0) {
+      return false;
+    }
 }
+return true;
+} 
 
 module.exports = trialDivision;
