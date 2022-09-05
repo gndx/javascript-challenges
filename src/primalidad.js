09ffb330108c830727ce334b94b5fa510a780e63
laura-p-juzto <laura.pardo@juzto.co>
2022-09-05 14:39:44 -0500

    Solutions

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,13 @@
 const trialDivision = (number) => {
-  // your code here
+  if (number % 1 != 0) return false;
+  if (number <=1) return false;
+  if (number=== 2) return true;
+  var sr = Math.sqrt(number);
+  for(var i=2;i<=sr;i++)
+  {
+    if(number%i==0) return false;
+  }
+  return true;
 }
 
 module.exports = trialDivision;
