c3d49b1919dcfc9b95a8305223dbd441ce09b7f2
Alberto_x64 <alberto.arias.x64@gmail.com>
2022-09-06 07:03:20 -0500

    ready all tasks

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,11 +1,11 @@
 const fibonacci = (number) => {
-  // your code here
-  let fibonacci_result = [1]
-  for (let index = 1; index < number; index++) {
-    if (fibonacci_result.length == 1) fibonacci_result.push(1)
-    else fibonacci_result.push(fibonacci_result[fibonacci_result.length-1] + fibonacci_result[fibonacci_result.length-2])
-  }
-  return fibonacci_result
+	// your code here
+	let fibonacci_result = [1]
+	for (let index = 1; index < number; index++) {
+		if (fibonacci_result.length == 1) fibonacci_result.push(1)
+		else fibonacci_result.push(fibonacci_result[fibonacci_result.length - 1] + fibonacci_result[fibonacci_result.length - 2])
+	}
+	return fibonacci_result
 }
 
 module.exports = fibonacci;
