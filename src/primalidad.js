33da8f4fdfba213fe589360ce7b83ec4211c39df
carlosmperilla <105413945+carlosmperilla@users.noreply.github.com>
2022-09-05 21:03:10 -0300

    Calculo de primalidad usando la Criba de Eratostenes.

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,27 @@
 const trialDivision = (number) => {
-  // your code here
+   if (typeof(number) !== 'number') {
+         // Generamos e informamos el error para datos no númericos.
+        const NON_NUMERIC_ERROR = "El dato ingresado no es un número."
+        console.error(NON_NUMERIC_ERROR)
+        return Error(NON_NUMERIC_ERROR)
+    } else {
+         // Para los números negativos. En caso de que se quiera realizar algun proceso alternativo.
+        // Indefinido por ahora.
+        if (number <= 0){
+            return undefined
+        }
+
+        // Usamos la Criba de Eratostenes.
+        let limite = Math.floor(Math.sqrt(number))
+        let n_aux = 2;
+        while (n_aux <= limite){
+          if ((number % n_aux) == 0) {
+            return false
+          }
+          n_aux++
+        }
+        return true
+    }
 }
 
 module.exports = trialDivision;
