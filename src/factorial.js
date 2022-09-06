223930dd95b3e0df086a334237d02bef7beac4fe
Carlos Navia <cenaviat@gmail.com>
2022-09-05 23:44:36 -0500

    challenge resolved

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,15 @@
 const factorial = (number) => {
   // your code here
+  let resultado = 1;
+    let i = 1;
+
+    while(i <= number) {
+        resultado *= i;
+
+        ++i;
+    }
+
+    return resultado;
 }
 
 module.exports = factorial;
