d87bbe5cf5417f6b156c5e7d0deb853561e6c689
plopez10 <lopezpablo010@gmail.com>
2022-09-06 11:06:21 -0600

    Completing JavaScript Challenges

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,18 @@
 const trialDivision = (number) => {
   // your code here
+  if (Number.isInteger(number)) {
+    if (number <= 1) {
+      return false;
+    }
+    for (var i = 2, x = Number.parseInt(Math.sqrt(number)) + 1; i < x; i += 1) {
+      if (number % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = trialDivision;
