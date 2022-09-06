3c2e0e1fa691a430c3d09ecb91293648ab97e09c
Cesar Silva <cesar.silva@softwareone.com>
2022-09-06 13:02:27 -0500

    Solution challenges

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,9 @@
 const fibonacci = (n) => {
-  // your code here
+  const numbers = [0, 1];
+  for (let index = 1; index < n; index++) {
+    let value = numbers[index] + numbers[index - 1];       
+    numbers.push(value);    
+  }
+  return numbers.splice(1);
 }
-
 module.exports = fibonacci;
