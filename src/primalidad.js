f98efc7c352c216680ed986940ff953468510e95
“Esteban-Somoza” <estfersomoza@gmail.com>
2022-09-06 12:27:03 -0300

    ejercicios factorial y primalidad

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,15 @@
 const trialDivision = (number) => {
-  // your code here
+  let isPrime = true
+  // if (number === 2) isPrime = true
+  if (number > 1) {
+    for (let i = 2; i < number; i++) {
+      if (number % i == 0) {
+        isPrime = false
+      }
+    }
+  }
+  // else return false
+  return isPrime
 }
 
 module.exports = trialDivision;
