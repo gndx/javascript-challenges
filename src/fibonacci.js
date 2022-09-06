ffe14cd0cb855a26920e755b5f2efc7a49353d7a
migurd <angelquimorita@gmail.com>
2022-09-05 20:14:59 -0600

    Ejercicios terminados!

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,9 @@
 const fibonacci = (n) => {
-  // your code here
+  let serie = [0, 1];
+  for (let i = 2; i <= n; i++)
+    serie.push(serie[i-1] + serie[i-2]);
+  serie.shift() // To delete the 0
+  return serie;
 }
 
 module.exports = fibonacci;
