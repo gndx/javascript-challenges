95d64eb4b62e2104d71fdfe6d1940fe0a4804fb1
Wellinton del Orbe <wellintonleuis@gmail.com>
2022-09-05 22:09:58 -0400

    Solucion Primer reto JavaScript-Challenges

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,6 @@
 const factorial = (number) => {
   // your code here
+  return number == 0 || number == 1 ? 1 : number * factorial(number-1);
 }
 
 module.exports = factorial;
--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,12 @@
 const fibonacci = (n) => {
   // your code here
+  let number1, number2, resul;
+  for(let i = 0; i<n; i++){
+    resul = number1 + number2;
+    number1 = number2;
+    number2 = resul;
+    return resul;
+  }
 }
 
 module.exports = fibonacci;
