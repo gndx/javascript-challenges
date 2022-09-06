f6e17b0d874b2de188ad0a1254c0df7ce9d69c6c
johana rocio <u19306483@utp.edu.pe>
2022-09-06 14:46:49 -0500

    Reto Cumplido

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,17 @@
 const factorial = (number) => {
-  // your code here
+  
+  var total = number;
+
+  if (number === 1 || number === 0) 
+    return 1; 
+
+  while (number > 1) { 
+    number--;
+    total *= number;
+  }
+
+  return total;
+  
 }
 
 module.exports = factorial;
--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,24 @@
 const trialDivision = (number) => {
-  // your code here
+  
+if(number % 1 == 0){
+    
+    for (var i = 2; i < number; i++) {
+      if (number % i === 0) {
+        return false;
+      }
+
+    }
+
+    if (number === -1 || number === 0 || number === 1) {
+      return false;
+    } else {
+      return true;
+    }
+
+  } else {
+    return false;
+  }
+
 }
 
 module.exports = trialDivision;
