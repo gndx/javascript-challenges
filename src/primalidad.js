afdece5fdc3cc73b5820e077af90ea2dd8f1740f
Alberto_x64 <alberto.arias.x64@gmail.com>
2022-09-06 07:04:46 -0500

    ready all tasks

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,8 +1,8 @@
 const trialDivision = (number) => {
 	// your code here
 	if (number === 1 || number <= 0 || number % 1 != 0) return false
-	for (i = 2; i < number; i++) {
-		if (number % i == 0) return false
+	for (index = 2; index < number; index++) {
+		if (number % index == 0) return false
 	}
 	return true
 }
