1cb4fce8830972da63ed518311d4f7df0f950449
camilogonzalez543 <88495572+camilogonzalez543@users.noreply.github.com>
2022-09-06 07:08:48 -0500

    Update factorial.js

--- src/factorial.js
+++ src/factorial.js
@@ -1,10 +1,8 @@
 const factorial = (number) => {
-  number =5;
   let valor = number; 
   for( let i = number-1; i > 0; i--  ) {
     let multiplicacion = valor * i;
     valor = multiplicacion;
   }
-alert("multiplicacion "+ valor );
 }
 module.exports = factorial;
