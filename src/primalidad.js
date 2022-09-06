3c3ee6db415e7530fcaf44bce7b55e146faab9b9
diegocarlosdiaz <diegocarlosdiaz5@gmail.com>
2022-09-06 00:33:30 -0300

    solucion final de errores

--- src/primalidad.js
+++ src/primalidad.js
@@ -2,14 +2,18 @@ const trialDivision = (n) => {
 let raiz = Math.floor(Math.sqrt(n)) + 1;
 
 for(let i = 2; i < raiz; ++i) {
-    if (n % i == 0) {
+    if (n % i == 0 ) {
         return false;
     }
+}
+
+if(n==1){
+    return false;
 }
 
 return true;
 }
 
-/* console.log(trialDivision(5)); */
+ console.log(trialDivision(2)); 
 
 module.exports = trialDivision;
