eb99fb49b89c8c97c1dbf6555c57127d28220152
Gaspar Pelayo <gasparpelayo426@gmail.com>
2022-09-06 16:55:38 -0300

    My resolution.

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,9 @@
 const fibonacci = (n) => {
-  // your code here
+  let listNumbers = [0, 1];
+  for (let index = 2; index < n; index++) {
+    listNumbers.push(listNumbers[index - 1] + listNumbers[index-2]);
+  }
+  return listNumbers;
 }
 
 module.exports = fibonacci;
