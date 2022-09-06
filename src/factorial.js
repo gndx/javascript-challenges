e066f594fa7867c3c355f527e91d02ed475566a5
Luis Miguel Osorio Marin <68092335+MigueOsorio08@users.noreply.github.com>
2022-09-06 16:20:58 -0500

    Update factorial.js

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,16 @@
 const factorial = (number) => {
   // your code here
+  function factorial (num){
+    if(num <= 0){
+         console.log("Lo sentimos, el numero debe ser mayor que cero");
+    }else{
+         let factorial = num;
+         for(let i = (num - 1); i > 1; i--){
+              factorial *= i;
+         }
+         return `El factorial de ${num} es: ${factorial}`
+    }
+}
 }
 
 module.exports = factorial;
