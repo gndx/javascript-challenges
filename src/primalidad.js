675e2a5b586c9aafb255e7556881a925371006cc
felipe636 <juanpa_zafra985@hotmail.com>
2022-09-06 10:16:34 -0500

    solutions for the challenge for to be blockchain developer in platzi

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,25 @@
 const trialDivision = (number) => {
   // your code here
+  if(number<=1){
+    return false;
+  }else{
+    //Create a count for know the quantity numbers is divisors with the number
+    let count = 0;
+    for(let i=2; i<=number; i++){
+      if(number%i==0){
+        //quantity
+        count += 1;
+      }
+    }
+    //Check about the quanty numbers if the number just have a two divisor
+    //for example 1 and the same number, the number is prime.
+    if(count<=2){
+      return true;
+    }else{
+      return false;
+    }
+  }
 }
+
 
 module.exports = trialDivision;
