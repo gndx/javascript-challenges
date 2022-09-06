39fbbf2e6326ef0d322ac71ce5fa84cf61164b8a
jpuentevel <jpuentevelasquez@gmail.com>
2022-09-05 23:41:01 -0500

    test finalizado

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,54 @@
 const factorial = (number) => {
-  // your code here
+  /*
+  Creamos un array de números donde estarán todos los
+  números anteriores al número digitado por el usuario
+  */
+  let numeros = [];
+
+  /*
+  Variable donde se irá guradando el número anterior para
+  después guardarse en el arreglo.
+  */
+  let nAnterior = number;
+
+  /*
+  Variable donde guardaremos el que será el número factorial
+  resultante
+  */
+  let nFactorial=1;
+
+  /*
+  El primer número en guardarse en elarreglo debe ser el
+  digitado por el usuario.
+  */
+  numeros.push(number);
+
+  /*
+  ciclo donde se recorrerá desde el número hasta 0 para
+  determinar todos los anteriores
+  */
+  for(let i=number; i>0; i--) {
+    nAnterior = nAnterior - 1;
+    numeros.push(nAnterior);
+  }
+
+  /*
+  Eliminamos el número 0 del arreglo.
+  */
+  numeros.pop();
+
+  /*
+  Recorremos el array multiplicando todos los números del arreglo
+  para hallar el factorial.
+  */
+  numeros.forEach(e => {
+    nFactorial = nFactorial * e;
+  });
+
+  /*
+  Retornamos el factorial del número.
+  */
+  return nFactorial;
 }
 
 module.exports = factorial;
--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,36 @@
 const fibonacci = (n) => {
-  // your code here
+  
+  /*
+  Creamos el arreglo donde estará la secuencia
+  de Fibonacci.
+  */
+  let secuenciaFibonacci = [];
+
+  /*
+  Definimos las variables que nos servirán de
+  auxiliares para ir realizando las sumas.
+  */
+  let n1=1, n2=1, nProximo;
+
+  /*
+  Definimos un ciclo desde 1 al valor digitado.
+  Dentro del ciclo, guardamos el primer valor
+  con el que llegue n1, y la suma de este con
+  n2 será el próximo número. Luego intercambiamos
+  los valores para la siguiente repetición.
+  */
+  for (let i=1; i<=n; i++) {
+    secuenciaFibonacci.push(n1);
+    nProximo = n1 + n2;
+    n1 = n2;
+    n2 = nProximo;
+  }
+
+  /*
+  Después de lo anterior, solo nos queda retornar
+  la secuencia de Fibonacci en forma de arreglo.
+  */
+  return secuenciaFibonacci;
 }
 
 module.exports = fibonacci;
