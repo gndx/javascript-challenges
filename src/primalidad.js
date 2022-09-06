6bc0967080c052dea4b4e0db509c79d481141272
Frank Ramirez <89478677+fjramirezg@users.noreply.github.com>
2022-09-06 00:10:56 -0500

    Add files via upload

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,15 @@
 const trialDivision = (number) => {
-  // your code here
-}
+    if (number === 2) {
+        return true;
+    } else if (number < 2) {
+        return false;
+    }
+    for (let i = 2; i < number; i++) {
+        if (number % i === 0) {
+            return false;
+        }
+    }
+    return true;
+};
 
 module.exports = trialDivision;
