411c698bb3de1a7772c42e70996bf0b485ae2f4c
LuisBarco <luis.barco.inf@gmail.com>
2022-09-06 04:19:03 -0500

    Con fe hacia la beca

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,18 @@
-const factorial = (number) => {
-  // your code here
+const factorial = (number) => { 
+  let factorial2=1;
+
+  if  (number == 0 ){
+    factorial2=1;
+  }
+  else {
+    if (number>0){
+      while(number>=1){
+        factorial2=factorial2*number;
+        number=number-1;
+      }
+    }
+  }
+  return factorial2;
 }
 
 module.exports = factorial;
