9c414b04af1e0b348ad6e8145156e6ed1d1aa172
Felipe Flores <felipe_1357@outlook.com>
2022-09-05 21:46:03 -0500

    Update primalidad.js

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,36 @@
 const trialDivision = (number) => {
   // your code here
+  let control = true;
+  let i = number - 1;
+  let res;
+
+  while(control){
+    if(number % 1 == 0){
+    if(number == 1){
+      res = false;
+      control = false;
+    }else if(number > 1){
+      if(i > 1){
+        if(number % i == 0){
+          res = false;
+          control = false;
+        }else{
+          i--
+        }
+      }else{
+        res = true;
+        control = false;
+      }
+    }else{
+      res = false;
+      control = false;
+    }
+  }else{
+    res = false;
+    control = false;
+  }
+  }
+  return res;
 }
 
 module.exports = trialDivision;
