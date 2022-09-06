29e27d615443c6ed8af104b0f34a3b085faea377
Missael <missaelwlsn@gmail.com>
2022-09-05 23:18:19 -0600

    retos resueltos

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,10 @@
 const factorial = (number) => {
   // your code here
+  let f = 1;
+  for(var i = 1;i<number;i++ ){
+      f *=i+1;
+  }
+  return f;
 }
 
 module.exports = factorial;
--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,14 @@
 const fibonacci = (n) => {
   // your code here
+  let elemento = new Array();
+  for (let index = 0; index < n; index++) {
+     if(index <= 1){
+      elemento[index] = 1;
+     }else{
+      elemento[index] = (elemento[index-1])+(elemento[index-2]);
+     }  
+  }
+  return elemento;
 }
 
 module.exports = fibonacci;
