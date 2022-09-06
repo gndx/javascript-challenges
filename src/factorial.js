e2e78a1cab371c79abfb075c7a3aac914d47770c
Felipe Torrejon <felipetorrejon@live.cl>
2022-09-06 02:07:29 -0400

    reto finalizado

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,14 @@
 const factorial = (number) => {
-  // your code here
+  let fct = 1;
+
+  if (number < 0){
+    return "Error: enter a non-negative integer";
+  }
+  if (number > 0) {
+    fct = number * factorial(number - 1);
+  }
+
+  return fct;
 }
 
 module.exports = factorial;
--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,17 @@
 const fibonacci = (n) => {
-  // your code here
+  let arr = [];
+  if ( n <= 0 ){
+    return "Error: enter a positive integer"
+  } else {
+    arr.push(1);
+    if (n > 1){
+      arr.push(1);
+      for (let i = 2; i < n; i++){
+        arr.push(arr[i-2] + arr[i-1]);
+      }
+    }    
+  }
+  return arr;
 }
 
 module.exports = fibonacci;
