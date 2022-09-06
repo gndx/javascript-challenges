e3f956ebb020864ea0570359525965936611d3a8
unknown <viviana.calva@lumation.co>
2022-09-06 13:38:33 -0500

    Añadiendo soluciones

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,13 @@
 const trialDivision = (number) => {
   // your code here
+  if (number !== Math.round(number)) return false;
+
+  const divisores = [];
+  for (let i = 2; i < 10; i++)
+    if (i !== number && number % i === 0) divisores.push(i);
+
+  // console.log(divisores);
+  return number > 1 && divisores.length === 0;
 }
 
 module.exports = trialDivision;
