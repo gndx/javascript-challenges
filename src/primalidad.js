ee33c170f0bd0bc5c921e75b50e8d8013475189c
stbedoyaa <91540848+stbedoyaa@users.noreply.github.com>
2022-09-06 07:29:37 -0500

    Update primalidad.js

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,9 +1,9 @@
 const trialDivision = (number) => {
-if (number < 0 || !Number.isInteger(number)) return false;
-if(number === 0 || number === 1 || number === 4) return false;
-for (let index = 2; index < number/2; index++) {
-  if (number % index === 0) return false;
-}
-return true;
+  // your code here
+const trialDivision = (num) => {
+  const number = Math.trunc(num)
+  for(let i = 2,raiz=Math.sqrt(number); i <= raiz; i++)
+    if(number % i === 0) return false;
+  return number > 1;
 }
 module.exports = trialDivision;
