832de0bf5b57ace533eafba49564c838b22b7aad
daniel-barreto <daniel30081990@gmail.com>
2022-09-05 21:47:53 -0400

    solution

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,9 @@
 const factorial = (number) => {
-  // your code here
+
+	if (number == 0){
+		return 1;
+	}
+	return number * factorial (number-1);
 }
 
 module.exports = factorial;
--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,13 @@
 const fibonacci = (n) => {
-  // your code here
+  var fib = [1, 1];
+  if ( n === 1 )
+    fib = [1];
+  for(var i=fib.length; i<n; i++) {
+    fib[i] = fib[i-2] + fib[i-1];
+  }
+
+  return fib;
+
 }
 
 module.exports = fibonacci;
