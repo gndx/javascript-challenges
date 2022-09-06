ab3620a9a5aa7f91fc2faa2e4ec4bbe7ca4c4c72
Darley <tiusdiux.com@gmail.com>
2022-09-05 20:49:39 -0500

    problems solved

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,17 @@
 const factorial = (number) => {
   // your code here
+  let result = number;
+  if (number > 1) {
+    number--;
+    result *= factorial(number);
+    return result;
+  }
+  else if (number < 0) {
+    return "Try another number..."
+  }
+  else {
+    return 1;
+  }
 }
 
 module.exports = factorial;
