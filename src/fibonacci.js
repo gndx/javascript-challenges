b612054c9ec2d75a9fd982d24b2935ecb34f434c
Wellinton Del Orbe <wellinotnleuis@gmail.com>
2022-09-05 22:41:38 -0400

    Arreglando la funcion Fiboncci

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,6 +1,6 @@
 const fibonacci = (n) => {
   // your code here
-  let number1, number2, resul;
+  let number1 = 1, number2 = 1, resul = 0;
   for(let i = 0; i<n; i++){
     resul = number1 + number2;
     number1 = number2;
