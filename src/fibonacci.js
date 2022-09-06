49a4c922ebb70c812de4e84a5d7aeac3e55782c8
Martin Bronzino <martin@dlab.com.ar>
2022-09-06 10:25:56 -0300

    Fibonacci

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,13 @@
 const fibonacci = (n) => {
-  // your code here
+  //PARA PASAR LOS TEST ELIMINAMOS EL VALOR 0 DEL ARRAY
+  let f_arr = [1];
+  if(n>1){
+    f_arr[1] = 1;
+    for (let i = 2; i < n; i++) {
+      f_arr[i] = f_arr[i - 2] + f_arr[i - 1];
+    }
+  }
+  return f_arr;
 }
 
 module.exports = fibonacci;
