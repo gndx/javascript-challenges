57271b8d1e23ef08bf53b7d1d9aa29fc78d43898
stbedoyaa <91540848+stbedoyaa@users.noreply.github.com>
2022-09-06 07:29:17 -0500

    Update fibonacci.js

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,9 +1,11 @@
 const fibonacci = (n) => {
-if (n === 1) return [1];
-arrayFibonacci = [1, 1];
-for (let index = 2; index < n; index++) {
-  arrayFibonacci[index] = arrayFibonacci[index - 1] + arrayFibonacci[index -2];
-}
-return arrayFibonacci;
+  // your code here
+const fibonacci = (n) => { 
+    if(n == 1) return [1] 
+    var fibonacci = [1 , 1];
+    for (var i = 2; i < n; i++) {
+      fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+    }
+    return fibonacci;    
 }
 module.exports = fibonacci;
