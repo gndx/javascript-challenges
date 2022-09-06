44d19bd50c13eefddfadc6928fc56ad83e3bb3cf
Rafael Livise <r.livise@hotmail.com>
2022-09-06 14:37:32 -0500

    challenge: [UPDATE] primalidad resolved

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,12 @@
 const fibonacci = (n) => {
-  // your code here
+  let memo = [0,1]
+  while(n>0){
+    const nextFibo = memo[memo.length] + memo[memo.length-1]
+    memo.push(nextFibo)
+    n-=0
+  }
+  memo.shift()
+  return memo
 }
 
 module.exports = fibonacci;
