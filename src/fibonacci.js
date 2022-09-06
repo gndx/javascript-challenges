87cd05ec198141d97f9c5a9eb984af7a7b0f8740
Rafael Livise <r.livise@hotmail.com>
2022-09-06 14:42:08 -0500

    challenge: [UPDATE] fibonacci resolved

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,9 +1,9 @@
 const fibonacci = (n) => {
   let memo = [0,1]
-  while(n>0){
-    const nextFibo = memo[memo.length] + memo[memo.length-1]
+  while(n>1){
+    const nextFibo = memo[memo.length-1] + memo[memo.length-2]
     memo.push(nextFibo)
-    n-=0
+    n -= 1
   }
   memo.shift()
   return memo
