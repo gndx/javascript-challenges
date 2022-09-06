77948828263ddda32c0051a712c302aad2bf1522
JoarDev <j.ramos5452@gmail.com>
2022-09-06 11:25:45 -0300

    test complete

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,9 @@
 const factorial = (number) => {
-  // your code here
+    var factorial=1;
+             
+    for (var i = 2; i <= number; i++)
+        factorial = factorial * i;
+    return factorial;
 }
 
 module.exports = factorial;
