8094bcebb3a29b47788be383ce29b3788cd5e768
Nayru <nanualexandraramirez@gmail.com>
2022-09-06 10:00:27 -0500

    fibonacci passed all test

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,7 +1,13 @@
 const fibonacci = (n) => {
-  if (n <= 1) return n;
-
-  return fibonacci(n-1) + fibonacci(n-2);
+  if ( n == 1 ){
+    return [1]
+  } else {
+    let result = [1, 1];
+    for (var i = 2; i < n; i++) {
+      result.push(result[i-2] + result[i-1]);
+    }
+    return result;
+  }
 }
 
 module.exports = fibonacci;
