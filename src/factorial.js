b18a5d136ff3020771b84d485d4db3f7fef460ee
snowingt <45589202+snowingt@users.noreply.github.com>
2022-09-06 04:56:15 +0100

    javascript-challenges-resuelto

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,16 @@
 const factorial = (number) => {
   // your code here
+  let total = 1; 
+  for (i=1; i<=number; i++) {
+    total = total * i; 
+    console.log(total)
+  }
+  return total; 
 }
 
 module.exports = factorial;
+
+const numb = 5;
+
+
+factorial(numb)
