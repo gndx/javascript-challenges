ed14adc515c25980bfdc2123737ec98776c35658
genaroibc <bonavitaismael@gmail.com>
2022-09-06 10:42:04 -0300

    [FEAT] add fibonacci function

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = (n) => {
-  // your code here
-}
+const fibonacci = n => {
+  if (n < 1) return [0];
+
+  const resultsArr = [0, 1];
+  let prev = 0;
+  let current = 1;
+
+  while (current !== n) {
+    current = prev + current;
+    if (current > n) return resultsArr;
+    resultsArr.push(current);
+    prev = current - prev;
+  }
+
+  return resultsArr;
+};
 
 module.exports = fibonacci;
