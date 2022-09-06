a577b128a2c9fd7e4a3a356a2e0c5c06cb07c7cb
carlosp81 <carlospt_dev@outlook.com>
2022-09-06 15:49:24 -0500

    fibonacci test passed !

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,19 @@
 const fibonacci = (n) => {
-  // your code here
+    let fibonacci_array = [];
+    if (n === 1) {
+        fibonacci_array = [1];
+    } 
+    if (n === 2) {
+        fibonacci_array = [1, 1]; 
+    } 
+    let i = 2;
+    if (i < n) {
+        fibonacci_array = [1, 1]
+        while (i < n) {
+            fibonacci_array[i] = fibonacci_array[i-1] + fibonacci_array[i-2]
+            i+=1
+        }
+    }
+    return fibonacci_array
 }
-
 module.exports = fibonacci;
