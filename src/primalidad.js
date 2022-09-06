ab6a02873ad4f82a581a6c7edb8578edad845940
Nayru <nanualexandraramirez@gmail.com>
2022-09-06 08:53:57 -0500

    primalidad solved

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,15 @@
 const trialDivision = (number) => {
-  // your code here
+  if (number == 2 || number == 3 || number == 5 || number == 7) {
+    return true;
+  } else if ((number < 2) || (number % 2 == 0)) {
+    return false;
+  } else {
+    for (let i = 3; i <= Math.sqrt(number); i += 2) {
+      if (number % i == 0)
+        return false;
+    }
+    return true;
+  }
 }
 
 module.exports = trialDivision;
