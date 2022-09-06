2fe12e2de4917403ef67967fa698ff6a65956690
PaolaGomez79 <paomeli79@gmail.com>
2022-09-06 17:17:00 -0300

    Realizo Challenge

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,11 @@
 const factorial = (number) => {
   // your code here
+  let factorial = 1;
+
+  for(let n = 1; n <= number; n++) {
+    factorial *= n;
+  }
+  return factorial;
 }
 
 module.exports = factorial;
--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,15 @@
 const fibonacci = (n) => {
   // your code here
+  
+  let fibonacci = [1, 1];
+
+  if(n == 1)  fibonacci = [fibonacci[0]]; 
+ 
+  for (let i = 2; i < n; i++) {
+    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+  }
+
+  return fibonacci;
 }
 
 module.exports = fibonacci;
