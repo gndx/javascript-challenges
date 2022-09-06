aa98dc22a27c572e576aad26742fc67f782b3c9c
Manuel Silva <manuel.silvag1@gmail.com>
2022-09-05 21:18:49 -0500

    Challenge complete

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,13 @@
 const trialDivision = (number) => {
   // your code here
-}
+  const absNumber = Math.abs(number);
+  if (!Number.isInteger(absNumber)) return false;
+  if (absNumber <= 1) return false;
+  if (absNumber > 2 && absNumber % 2 === 0) return false;
+  for (let i = 3; i < absNumber / 3; i += 2) {
+    if (absNumber % i === 0) return false;
+  }
+  return true;
+};
 
 module.exports = trialDivision;
