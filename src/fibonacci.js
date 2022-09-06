0a9304b67668f3746305952518273e33eec6166f
ascife <bizapro123@gmail.com>
2022-09-05 19:21:49 -0500

    Challenge

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,15 @@
 const fibonacci = (n) => {
   // your code here
+  let secuence = [0, 1];
+  let prevIndex = 0, nextIndex = 1;
+
+  for(let i = 1; i <= n; i++) {
+    secuence.push(secuence[prevIndex] + secuence[nextIndex]);
+    prevIndex++;
+    nextIndex++;
+  }
+
+  return secuence.slice(1, n + 1);
 }
 
 module.exports = fibonacci;
--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,14 @@
 const trialDivision = (number) => {
   // your code here
+  const isDecimal = number % 1;
+
+  if(isDecimal) 
+    return false;
+
+  for(let i = 2, s = Math.sqrt(number); i <= s; i++)
+    if(number % i === 0) return false; 
+  
+  return number > 1;
 }
 
 module.exports = trialDivision;
