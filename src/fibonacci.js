dcf2871d5be32b67e5de62f83a3f8fe2e20babaa
Brayan Quiroz <brayan.quiroz@kavak.com>
2022-09-06 23:37:54 -0500

    feat: :star:  platzi challenge functionality

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,11 @@
 const fibonacci = (n) => {
   // your code here
-}
+  let listNumber = [];
+  for (let i = 0; i < n; i++) {
+    if (i <= 1) listNumber[i] = 1;
+    else listNumber[i] = listNumber[i - 2] + listNumber[i - 1];
+  }
+  return listNumber;
+};
 
 module.exports = fibonacci;
--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,11 @@
 const trialDivision = (number) => {
   // your code here
-}
+  number = Math.trunc(number);
+  if (number <= 1) return false;
+  for (var i = 2; i <= number - 1; i++) {
+    if (number % i == 0) return false;
+  }
+  return true;
+};
 
 module.exports = trialDivision;
