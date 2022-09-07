7f0d0171e621cdc33e734d00243bdf24dac47fed
Axel Reyes <rbaxel.f@gmail.com>
2022-09-06 19:15:58 -0500

    Step 5) Adding challenge code resolutions

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,20 @@
 const fibonacci = (n) => {
-  // your code here
+
+const sucesion=[0,1]
+
+
+	if(n<2){
+        	sucesion.shift();
+                return sucesion;
+        }else{
+        	for(var i=2;i<=n;i++){
+                	sucesion[i] = sucesion[i-2]+sucesion[i-1]
+                }
+         sucesion.shift();
+         return sucesion;
+        }
+
+
 }
 
 module.exports = fibonacci;
