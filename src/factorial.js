961e9adf38b3fde20d06a714f96a393310da111c
Brayan Quiroz <brayan.quiroz@kavak.com>
2022-09-06 23:40:19 -0500

    Revert "feat: :star:  platzi challenge functionality"
    This reverts commit dcf2871d5be32b67e5de62f83a3f8fe2e20babaa.

--- src/factorial.js
+++ src/factorial.js
@@ -1,7 +1,5 @@
 const factorial = (number) => {
   // your code here
-  if (number < 2) return 1;
-  return number * factorial(number - 1);
-};
+}
 
 module.exports = factorial;
--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,11 +1,5 @@
 const fibonacci = (n) => {
   // your code here
-  let listNumber = [];
-  for (let i = 0; i < n; i++) {
-    if (i <= 1) listNumber[i] = 1;
-    else listNumber[i] = listNumber[i - 2] + listNumber[i - 1];
-  }
-  return listNumber;
-};
+}
 
 module.exports = fibonacci;
