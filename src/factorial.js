7a111df85572b162bbce30b8ec0124c5c2c0526b
Alexander Marquez <alex18escorpio@gmail.com>
2022-09-06 23:29:41 -0500

    solve exercise

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,7 @@
 const factorial = (number) => {
-  // your code here
-}
+  if (number === 0) return 1;
+  else if (number < 0) return -1;
+  else return number * factorial(number - 1);
+};
 
 module.exports = factorial;
--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,12 @@
 const trialDivision = (number) => {
-  // your code here
-}
+  let bandera = true;
+  if (number <= 1) return false;
+  if (!Number.isInteger(number)) return false;
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) bandera = false;
+  }
+  return bandera;
+};
+trialDivision(5);
 
 module.exports = trialDivision;
