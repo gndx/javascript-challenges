bfc41a337c8cb9cb7045365d90b1126157223e1c
francisco-solis99 <franciscosoliscss@gmail.com>
2022-09-07 18:55:30 -0500

    [Make] the challengues

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,11 @@
 const factorial = (number) => {
   // your code here
+  let factorial = 1;
+  while (number > 0) {
+    factorial *= number;
+    number -= 1;
+  }
+  return factorial;
 }
 
 module.exports = factorial;
