b29f4a2da4109b7a26e1cb11b2b5f73f85b8a2fe
Jefrien Armando Alvizures Martínez <jefrienalvizures@gmail.com>
2022-09-06 18:24:29 -0600

    Challenges Completed

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,10 @@
 const fibonacci = (n) => {
   // your code here
+  let res = [0, 1]
+  for (let i = 2; i <= n; i++) {
+    res[i] = res[i - 2] + res[i - 1]
+  }
+  return res.slice(1)
 }
 
 module.exports = fibonacci;
