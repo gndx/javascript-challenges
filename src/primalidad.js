5fd1cc5d86e9a8f79477e03aaa7eb26bb68d9064
Fabrizio Ordoñez <contacto@fabrizio.pro>
2022-09-08 00:18:10 -0500

    primality test ✅

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,8 @@
 const trialDivision = (number) => {
-  // your code here
+  if (number % 1 !== 0) return false;
+  for (let i = 2; i <= Math.trunc(Math.sqrt(number)); i++)
+    if (number % i === 0) return false;
+  return number >= 2;
 }
 
 module.exports = trialDivision;
