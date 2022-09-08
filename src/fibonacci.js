d71fcee36afdc5279fe217c650651f2b496eb27e
cmarchese <cmarchesetdiii@gmail.com>
2022-09-08 02:07:06 -0300

    Challenge for the rust program running properly. test_passed.png is the test run to see that everything was working ok

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,19 @@
 const fibonacci = (n) => {
-  // your code here
+  let fib = [];
+  for (let i = 0; i < n; i++) //[1,1+]
+  {
+  	if((i-2)<0)
+  	{
+  		fib.push(1);
+  	}
+  	else
+  	{
+  		fib.push(fib[i-2]+fib[i-1]);
+  	}
+  }
+
+  return(fib);
+
 }
 
 module.exports = fibonacci;
