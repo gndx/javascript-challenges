415a054ba06f6f1adc118781f6c30edea84859e5
Roger David Alba Ortega <rogerdavidalbaortega@gmail.com>
2022-09-08 23:17:11 -0500

    Solution of the primalidad by passing all the tests

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,31 @@
 const trialDivision = (number) => {
-  // your code here
+  const noFloat= number%1===0;
+  let isCousin=false;
+  let count=0;
+  if(number>1 && noFloat){
+    for(let i=1;i<=number;i++){
+      const numOdd=(number%i === 0);
+
+      if(numOdd){
+        count+=1;
+      }else{
+        count=count;
+      }
+      if(count>2){
+        isCousin=false;
+      }else{
+        isCousin=true
+      }
+
+    }
+
+  }else{
+    isCousin=false;
+  }
+  console.log(isCousin)
+  return isCousin
+
 }
+trialDivision(5)
 
 module.exports = trialDivision;
