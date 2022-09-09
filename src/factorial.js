2b86fc192846671056cfce18c9a4542d4f50f4e6
Roger David Alba Ortega <rogerdavidalbaortega@gmail.com>
2022-09-08 23:16:47 -0500

    Solution of the factorial by passing all the tests

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,25 @@
 const factorial = (number) => {
-  // your code here
+  let factorial=0;
+
+  if(number===0){
+    return 1;
+  }else{
+
+    for(let i=0;i<number;i++){
+      let decrement=number-i;
+      if(i){
+        factorial= decrement*factorial
+        numMinor=decrement;
+      }else{
+        factorial=decrement;
+      }
+      
+  
+    }
+    return factorial
+  }
+
 }
+
 
 module.exports = factorial;
