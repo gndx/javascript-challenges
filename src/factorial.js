fc8adfc6f879bfa17381b323b95491e22ce0a575
Jose Prendiz <jprendiz@gmail.com>
2022-09-09 10:19:23 -0600

    Reto cumplido

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,12 @@
 const factorial = (number) => {
   // your code here
+  if (number === 0 || number === 1) {
+    return 1
+  }
+  else {
+    return(number * factorial(number - 1))
+  }
+  
 }
 
 module.exports = factorial;
--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,12 @@
 const fibonacci = (n) => {
   // your code here
+  if (n === 0 || n === 1) {
+    return n
+  }
+  else {
+    return(fibonacci(n-1) * fibonacci(n - 2))
+  }
+  
 }
 
 module.exports = fibonacci;
