8b0d03d96a9c9cde2aa1562128b54a93f1247bd3
Roger David Alba Ortega <rogerdavidalbaortega@gmail.com>
2022-09-09 22:57:42 -0500

    Solution of the fibonacci by passing all the tests

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,18 @@
 const fibonacci = (n) => {
-  // your code here
+  let numFibonacci= [];
+  if(n ===1){
+    numFibonacci.push(1);
+  }else if(n > 1){
+    let numBefore = 0;
+    let numAfter = 1;
+    numFibonacci.push(numBefore+numAfter);
+    for(let i=1;i<n; i++){
+      numFibonacci.push(numBefore+numAfter);
+      numBefore=numAfter;
+      numAfter= numFibonacci[i];
+    }
+  }
+  return numFibonacci;
 }
-
+fibonacci(3)
 module.exports = fibonacci;
