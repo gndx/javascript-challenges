d09dd6fbf9930af786f44d70874a655dcd5da8ea
genaroibc <bonavitaismael@gmail.com>
2022-09-12 13:48:20 -0300

    [FEAT] add trial division function

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,14 @@
-const trialDivision = (number) => {
-  // your code here
-}
+const trialDivision = number => {
+  const factorsArr = [];
+  let factor = 2;
+
+  while (number > 1) {
+    if (number % factor === 0) {
+      factorsArr.push(factor);
+      number /= factor;
+    } else factor += 1;
+  }
+  return factorsArr.length === 1;
+};
 
 module.exports = trialDivision;
