3a7bfe70d8162901c099cb91d68987dc53f6dc0b
IvySaskia <ivysaskia@gmail.com>
2022-10-06 00:39:52 -0400

    ADD: Fibonacci array function

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,7 +1,10 @@
 const fibonacci = (n) => {
-  if (n == 0) return 0;
-  if (n == 1) return 1;
-  return fibonacci(n - 2) + fibonacci(n - 1)
+  if (n == 0) return [0];
+  if (n == 1) return [1];
+  if (n == 2) return [1, 1];
+  let fibonacciArray = fibonacci(n - 1);
+  fibonacciArray.push(fibonacciArray[n - 2] + fibonacciArray[n - 3]);
+  return fibonacciArray;
 }
 
 module.exports = fibonacci;
