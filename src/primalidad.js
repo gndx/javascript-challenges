0eb41ee5c2df44a031725f9f1234a26f1725180a
IvySaskia <ivysaskia@gmail.com>
2022-10-06 00:10:45 -0400

    FIX: Primalidad function

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,8 +1,8 @@
-const trialDivision = (number) => {
-  if (number <=1 ) return false;
-  for (let i = 2; i <= number-1; i++)
-    if (number % i == 0) return false;
-  return true;
+const trialDivision = (number) => { 
+  if (number % 1 !== 0) return false;
+  for (let i = 2; i < number; i++)
+    if (number % i === 0) return false;
+  return number >= 2;
 }
 
 module.exports = trialDivision;
